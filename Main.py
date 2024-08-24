f59feb2f913bc3fb37c16cc6bf02290cb587009a
Bhawesh Sinha <Bhaweshsinha100@gmail.com>
2024-08-24 17:11:45 +0530

    Create Main.py

---
+++ Main.py
@@ -0,0 +1,9 @@
+print('''
+<-------------------------------(Library-Management System)------------------>
+      Enter the task you want to perform:
+      1. Add book
+      2. Delete book
+      3. List all books
+      4. Rent the book
+      5. Get the details of the book
+''')
