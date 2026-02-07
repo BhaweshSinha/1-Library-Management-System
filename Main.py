1070614c72ab3ccd9cd669706c6a122256398125
Bhawesh Sinha <Bhaweshsinha100@gmail.com>
2026-02-07 18:08:27 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -12,7 +12,7 @@ user = int(input("Choose option number (Press 0 to exit) -> "))
 def add_book():
       user = input('Enter the book ID')
 def delete_book():
-      user = input()
+      user = input('Enter the book ID')
 def list_book():
       user = input()
 def rent_book():
